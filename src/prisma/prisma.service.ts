b6563475e5b5e11200b1e8a2dcd118966b79c7b8
trongdat.nguyen <trongdat.nguyen@dssolution.jp>
2025-11-25 09:40:07 +0700

    fix: prisma v7 bug

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,20 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from 'generated/prisma/client';
+import configuration from 'src/config/configuration';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    const connectionString = configuration().database;
+
+    const adapter = new PrismaPg({ connectionString });
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
